189cde3734b9cd65d09c7372b178dd46a61e27a3
svoana <oanacorobuta@yahoo.com>
2016-11-28 16:21:33 +0200

    Add C# program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            score = 15;
+            Console.WriteLine(score);
+
+            bool LevelComplete = true;
+            Console.WriteLine(LevelComplete);
+
+            string message = "Hello world";
+            Console.WriteLine(message);
+            Console.ReadKey();
+
+        }
+    }
+}
